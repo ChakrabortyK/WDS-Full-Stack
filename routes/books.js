72466df79bb29e4933c8bb4eeed1edcd7315e94e
ChakrabortyK <chakrabortyk21offc@gmail.com>
2023-09-05 20:20:14 +0530

    multer

--- routes/books.js
+++ routes/books.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
+
+
 const BookModel = require('../models/book')
 const AuthorModel = require('../models/author')
+
+const imageMimeTypes = ['image/jpeg', 'image/png', 'images/gif']
+const uploadPath = path.join('static',BookModel.coverImgBasePath)
+// console.log(uploadPath);
+const upload = multer({
+  dest: uploadPath,
+  fileFilter: (req, file, callback) => {
+    callback(null, imageMimeTypes.includes(file.mimetype))
+  }
+})
+
+
+
+
+
 
 //ALL BOOKS ROUTE
 router.get('/', async (req, res) => {
@@ -24,8 +43,24 @@ router.get('/new', async (req, res) => {
 
 
 //CREATE BOOKS ROUTE
-router.post('/new', async (req, res) => {
-  res.send("Post Books")
+router.post('/new',upload.single('cover'), async (req, res) => {
+  const filename = req.file != null ? req.file.filename : null
+  const book = new BookModel({
+    title: req.body.title,
+    author: req.body.author,
+    publishDate : new Date(req.body.publishDate),
+    pageCount: req.body.pageCount,
+    coverImgName: filename,
+    description: req.body.description
   })
+
+  try {
+    const newBook = await book.save();
+    // res.redirect(`/authors/${newAuthor.id}`)
+    res.redirect('/books')
+  } catch (err) {
+    
+  }
+})
 
 module.exports = router;
